5c0d67e32666e153bef9809862469f7ed71aafab
tobiasforest <rizhonglin@gmail.com>
2021-10-20 22:00:41 +0800

    First commit
    Added all files of the original project and formatted them.

---
+++ app.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# --------------------------------------------------------------
+#    Description：
+#
+#
+# --------------------------------------------------------------
+from login_logout import log_bp
+from data_get import data_bp
+from edit import edit_bp
+from teacher import teacher_bp
+from student import student_bp
+from admin import admin_bp
+from gevent import pywsgi
+from flask import Flask, redirect, url_for
+import sys
+sys.path.append('./src')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'xgmdRrFe.323'
+
+app.register_blueprint(admin_bp)
+app.register_blueprint(student_bp)
+app.register_blueprint(teacher_bp)
+app.register_blueprint(edit_bp)
+app.register_blueprint(data_bp)
+app.register_blueprint(log_bp)
+
+
+# 根路由
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    # print('here')
+    return redirect(url_for('log.login'))
+
+
+# 主函数
+if __name__ == '__main__':
+    # print(app.url_map)
+    # app.run(host='127.0.0.1', port = 5000,debug=True)
+    app.run(debug=True)
+    server = pywsgi.WSGIServer(('', 5000), app)
+    server.serve_forever()
---
+++ src/student.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# --------------------------------------------------------------
+#    Description：
+#
+#
+# --------------------------------------------------------------
+from flask import Flask, session, render_template, Blueprint
+
+student_bp = Blueprint('student', __name__, url_prefix='/student')
+
+
+@student_bp.route('/student_index', methods=['GET', 'POST'])
+def student_index():
+    posts = {"StudentNo": session['No'], "StudentName": session['name']}
+    return render_template('/student/student_index.html', posts=posts)
+
+
+@student_bp.route('/student_course_specialityCurriculum', methods=['GET', 'POST'])
+def student_course_specialityCurriculum():
+    posts = {"StudentNo": session['No'], "StudentName": session['name']}
+    return render_template('/student/student_course_specialityCurriculum.html', posts=posts)
+
+
+@student_bp.route('/student_course_studentCurriculum', methods=['GET', 'POST'])
+def student_course_studentCurriculum():
+    posts = {"StudentNo": session['No'], "StudentName": session['name']}
+    return render_template('/student/student_course_studentCurriculum.html', posts=posts)
+
+
+@student_bp.route('/student_course_selectCourse', methods=['GET', 'POST'])
+def student_course_selectCourse():
+    posts = {"StudentNo": session['No'], "StudentName": session['name']}
+    return render_template('/student/student_course_selectCourse.html', posts=posts)
+
+
+@student_bp.route('/student_exam_viewExam', methods=['GET', 'POST'])
+def student_exam_viewExam():
+    posts = {"StudentNo": session['No'], "StudentName": session['name']}
+    return render_template('/student/student_exam_viewExam.html', posts=posts)
+
+
+@student_bp.route('/student_score_viewScore', methods=['GET', 'POST'])
+def student_score_viewScore():
+    posts = {"StudentNo": session['No'], "StudentName": session['name']}
+    return render_template('/student/student_score_viewScore.html', posts=posts)
+
+
+@student_bp.route('/student_evaluate_addEvaluation', methods=['GET', 'POST'])
+def student_evaluate_addEvaluation():
+    posts = {"StudentNo": session['No'], "StudentName": session['name']}
+    return render_template('/student/student_evaluate_addEvaluation.html', posts=posts)
